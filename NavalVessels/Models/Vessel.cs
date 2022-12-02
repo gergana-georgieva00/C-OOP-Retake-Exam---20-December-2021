1ff30c5028013dce52d2d97ba5ebc8320e626e6c
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-02 13:22:30 +0200

    C# OOP Retake Exam - 20 Dec 2021

---
+++ NavalVessels/Models/Vessel.cs
@@ -0,0 +1,42 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public abstract class Vessel : IVessel
+    {
+        private string name;
+        private ICaptain captain;
+
+        public Vessel(string name, double mainWeaponCaliber, double speed, double armourThickness)
+        {
+            this.Name = name;
+            this.MainWeaponCaliber = mainWeaponCaliber;
+            this.Speed = speed;
+            this.ArmorThickness = armourThickness;
+        }
+
+        public string Name { get; private set; }
+
+        public ICaptain Captain { get; set; }
+        public double ArmorThickness { get; set; }
+
+        public double MainWeaponCaliber { get; private set; }
+
+        public double Speed { get; private set; }
+
+        public ICollection<string> Targets => throw new NotImplementedException();
+
+        public void Attack(IVessel target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RepairVessel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
