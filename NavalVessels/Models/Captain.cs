80322b3c51724b3a1861dfc1d3563baf85d370ef
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-02 15:10:53 +0200

    C# OOP Retake Exam - 20 Dec 2021

---
+++ NavalVessels/Models/Captain.cs
@@ -0,0 +1,48 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Captain : ICaptain
+    {
+        private string fullName;
+        private int combatExperience;
+        private List<IVessel> vessels;
+
+        public Captain(string fullName)
+        {
+            this.FullName = fullName;
+        }
+
+        public string FullName 
+        { 
+            get; 
+            private set; 
+        }
+
+        public int CombatExperience
+        {
+            get;
+            private set;
+        }
+
+        public ICollection<IVessel> Vessels => this.vessels.AsReadOnly();
+
+        public void AddVessel(IVessel vessel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncreaseCombatExperience()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
