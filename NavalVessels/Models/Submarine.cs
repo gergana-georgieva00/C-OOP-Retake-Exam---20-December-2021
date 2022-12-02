a5a97cb8d0ab90e075467301bb058b5be2ca07fc
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-02 15:06:26 +0200

    C# OOP Retake Exam - 20 Dec 2021

---
+++ NavalVessels/Models/Submarine.cs
@@ -0,0 +1,36 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Submarine : Vessel, ISubmarine
+    {
+        private const int initialArmorThickness = 200;
+
+        public Submarine(string name, double mainWeaponCaliber, double speed, double armourThickness) : base(name, mainWeaponCaliber, speed, armourThickness)
+        {
+            this.SubmergeMode = false;
+        }
+
+        public bool SubmergeMode { get; private set; }
+
+        public void ToggleSubmergeMode()
+        {
+            this.SubmergeMode = !this.SubmergeMode;
+
+            switch (SubmergeMode)
+            {
+                case true:
+                    this.MainWeaponCaliber += 40;
+                    this.Speed -= 4;
+                    break;
+                case false:
+                    this.MainWeaponCaliber -= 40;
+                    this.Speed += 4;
+                    break;
+            }
+        }
+    }
+}
