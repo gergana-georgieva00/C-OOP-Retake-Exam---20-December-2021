ab95d5828edb3560d3811bf034cd67e1055f16b6
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-02 15:25:22 +0200

    C# OOP Retake Exam - 20 Dec 2021

---
+++ NavalVessels/Repositories/VesselRepository.cs
@@ -0,0 +1,35 @@
+﻿using NavalVessels.Models.Contracts;
+using NavalVessels.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Repositories
+{
+    public class VesselRepository : IRepository<IVessel>
+    {
+        private List<IVessel> models;
+
+        public VesselRepository()
+        {
+            this.models = new List<IVessel>();
+        }
+
+        public IReadOnlyCollection<IVessel> Models => this.models.AsReadOnly();
+
+        public void Add(IVessel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVessel FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IVessel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
